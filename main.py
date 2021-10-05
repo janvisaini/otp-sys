55d19a26938c17bcbc7d5da1dff3c69e44720036
khushi467 <91981019+khushi467@users.noreply.github.com>
2021-10-05 23:31:21 +0530

    provide comments

--- main.py
+++ main.py
@@ -4,28 +4,26 @@ import datetime,os
 
 os.system("clear")
 
-otp=int(random.randint(1000000000,9999999999999))
+otp=int(random.randint(1000000,999999999))
 
 otp=list(str(otp))
 otpnew=""
 
 for i in range(6):
+    #adding the numbers
     otpnew.append(random.choice(otp)
 
 time=datetime.datetime.now().hour
 
 def greet():
-
+    # greet to user
     if time>=0 and time<12:
-
         print('Good morning!')
 
     elif time<=12 and time>16:
-
         print("Good afternoon!")
 
     else:
-
         print('Good evening!')
 
 
